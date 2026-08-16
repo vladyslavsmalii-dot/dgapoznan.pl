9b8c52083ac9219ef429ab9b888adf7b1468e772
vladyslavsmalii-dot <vladyslavsmalii@gmail.com>
2026-08-16 10:31:40 +0200

    Add files via upload

--- api/contact.js
+++ api/contact.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+﻿export default async function handler(req, res) {
     res.setHeader("Content-Type", "application/json; charset=utf-8");
 
     if (req.method !== "POST") {
