39018874c94bd37c40adcc5287f1fc5f95ead929
vladyslavsmalii-dot <vladyslavsmalii@gmail.com>
2026-08-16 11:00:09 +0200

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,5 +1,12 @@
 export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "https://dgapoznan.pl");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
 
     if (req.method !== "POST") {
         return res.status(405).json({
